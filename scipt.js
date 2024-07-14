ec728eb2bd0481ba36d4e3df1afe9ebeabcbce46
KATINCORP <mohaokatlego013@gmail.com>
2024-07-14 15:37:29 +0200

    Create scipt.js

---
+++ scipt.js
@@ -0,0 +1,8 @@
+// Get the CTA button element
+const learnMoreBtn = document.getElementById('learn-more-btn');
+
+// Add an event listener to the button
+learnMoreBtn.addEventListener('click', () => {
+	// Redirect to the services page
+	window.location.href = 'services.html';
+});
